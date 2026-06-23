e45a497c61dc6204aaa6b1386791c8b0c9d2e30c
osoTripolar <osotripolar@proton.me>
2026-06-23 16:30:25 -0500

    se añadio dotenv

--- src/config.js
+++ src/config.js
@@ -1,11 +1,14 @@
 import path from "node:path"
 import { fileURLToPath } from "node:url"
 
+import dotenv from "dotenv"
+
+dotenv.config()
+
 const __dirname = fileURLToPath(import.meta.url)
 
 // VARIABLES VARIAS
 export const ROOT = path.dirname(__dirname)
-
 
 // VARIABLES DE ENTORNO
 export const PORT = Number(process.env.PORT)
